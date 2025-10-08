dad0fcc6781470e4a16cf4ebeebc6f4055e677a5
M-Insure Marketing <marketing@m-insure.in>
2025-10-08 05:30:24 +0000

    it is not showing the name with a particular block of the chart but only

--- src/components/admin/analytics-charts.tsx
+++ src/components/admin/analytics-charts.tsx
@@ -167,7 +167,7 @@ export const AnalyticsCharts: React.FC<AnalyticsChartsProps> = ({ dateWiseSummar
                 fill="#8884d8"
                 content={<CustomizedTreemapContent />}
             >
-                <Tooltip formatter={(value: number, name: string) => [value.toFixed(2), name]}/>
+                <Tooltip formatter={(value: number, name: string) => [`${(value || 0).toFixed(2)}`, name]}/>
             </Treemap>
         </ChartCard>
       </div>
